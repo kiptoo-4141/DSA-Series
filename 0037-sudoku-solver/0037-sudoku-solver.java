2550956b1f6cafa7d0dd7961bcd8d6c35414cd8c
Moses Kipngetich Kiptoo <146611345+kiptoo-4141@users.noreply.github.com>
2025-08-13 10:28:06 +0300

    Update 0037-sudoku-solver.java

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,97 +1,105 @@
 class Solution {
-    // public void solveSudoku(char[][] board) {
-    //     solve(board);
-    // }
-    // private boolean solve(char[][] board){
-    //     for(int row = 0; row < 9; row++){
-    //         for(int col = 0; col <= 9; col++){
-    //             if(board[row][col] == '.'){
-    //                 for(char ch = '1'; ch <= '9'; ch++){
-    //                     if(isValid(board, row, col, ch)){
-    //                         board[row][col] = ch;
-    //                         if(solve(board)){
-    //                             return true;
-    //                         }
-    //                         board[row][col] = '.';
-    //                     }
-    //                 }
-    //                 return false;
-    //             }
-    //         }
-    //     }
-    //     return true;
-    // }
-    // private boolean isValid(char[][] board, int row, int col, char ch){
-    //     for(int i = 0; i < 9; i ++){
-    //         if(board[0][i] == ch){
-    //             return false;
-    //         }
-    //     }
-
-    //     for(int j = 0; j < 9; j ++){
-    //         if(board[j][0] == ch){
-    //             return false;
-    //         }
-    //     }
-    //     int rowSqrt = 3;
-    //     int colSqrt = 3;
-    //     int rowStart = row - row%rowSqrt;
-    //     int colStart = col - col%colSqrt;
-
-    //     for(int i = rowStart; i < rowSqrt; i++){
-    //         for(int j = colStart; j < colSqrt; j++){
-
-    //             if(board[i][j] == ch){
-    //                 return false;
-    //             }
-    //         }
-    //     }
-
-    //     return true;
-    // }
-
-
     public void solveSudoku(char[][] board) {
         solve(board);
     }
-
+    
     private boolean solve(char[][] board) {
+        // Find the first empty cell
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
-                // Look for empty cell
                 if (board[row][col] == '.') {
-                    // Try placing digits 1 to 9
-                    for (char num = '1'; num <= '9'; num++) {
-                        if (isValid(board, row, col, num)) {
-                            board[row][col] = num;
-
-                            // Recur and check if this leads to a solution
-                            if (solve(board)) return true;
-
-                            // Backtrack
+                    // Try digits 1-9
+                    for (char digit = '1'; digit <= '9'; digit++) {
+                        if (isValid(board, row, col, digit)) {
+                            board[row][col] = digit;
+                            
+                            // Recursively solve the rest
+                            if (solve(board)) {
+                                return true;
+                            }
+                            
+                            // Backtrack if solution not found
                             board[row][col] = '.';
                         }
                     }
-                    return false; // No number fits here
+                    // If no digit works, return false
+                    return false;
                 }
             }
         }
-        return true; // All cells are filled
-    }
-
-    private boolean isValid(char[][] board, int row, int col, char num) {
-        for (int i = 0; i < 9; i++) {
-            // Check row
-            if (board[row][i] == num) return false;
-
-            // Check column
-            if (board[i][col] == num) return false;
-
-            // Check 3x3 box
-            int boxRow = 3 * (row / 3) + i / 3;
-            int boxCol = 3 * (col / 3) + i % 3;
-            if (board[boxRow][boxCol] == num) return false;
-        }
+        // All cells filled successfully
         return true;
+    }
+    
+    private boolean isValid(char[][] board, int row, int col, char digit) {
+        // Check row constraint
+        for (int c = 0; c < 9; c++) {
+            if (board[row][c] == digit) {
+                return false;
+            }
+        }
+        
+        // Check column constraint
+        for (int r = 0; r < 9; r++) {
+            if (board[r][col] == digit) {
+                return false;
+            }
+        }
+        
+        // Check 3x3 box constraint
+        int boxRow = (row / 3) * 3;
+        int boxCol = (col / 3) * 3;
+        for (int r = boxRow; r < boxRow + 3; r++) {
+            for (int c = boxCol; c < boxCol + 3; c++) {
+                if (board[r][c] == digit) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
+
+// Example usage and test
+class SudokuSolverDemo {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        // Example puzzle
+        char[][] board = {
+            {'5','3','.','.','7','.','.','.','.'},
+            {'6','.','.','1','9','5','.','.','.'},
+            {'.','9','8','.','.','.','.','6','.'},
+            {'8','.','.','.','6','.','.','.','3'},
+            {'4','.','.','8','.','3','.','.','1'},
+            {'7','.','.','.','2','.','.','.','6'},
+            {'.','6','.','.','.','.','2','8','.'},
+            {'.','.','.','4','1','9','.','.','5'},
+            {'.','.','.','.','8','.','.','7','9'}
+        };
+        
+        System.out.println("Original puzzle:");
+        printBoard(board);
+        
+        solution.solveSudoku(board);
+        
+        System.out.println("\nSolved puzzle:");
+        printBoard(board);
+    }
+    
+    private static void printBoard(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            if (i % 3 == 0 && i != 0) {
+                System.out.println("------+-------+------");
+            }
+            for (int j = 0; j < 9; j++) {
+                if (j % 3 == 0 && j != 0) {
+                    System.out.print("| ");
+                }
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
